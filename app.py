9fb9cceb3f1148744abd55be31a5c32de02deefd
alishants <67666315+alishants@users.noreply.github.com>
2025-01-06 01:20:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import threading
+import asyncio
 
 # Flask-приложение для мини-приложения
 app = Flask(__name__)
@@ -22,6 +23,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Вот ваша ссылка: {google_maps_url}")
 
 def start_bot():
+    asyncio.set_event_loop(asyncio.new_event_loop())  # Создаём новый цикл событий для потока
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
@@ -29,5 +31,5 @@ def start_bot():
 
 # Запуск Flask и Telegram-бота параллельно
 if __name__ == "__main__":
-    threading.Thread(target=start_bot).start()  # Запуск Telegram-бота в отдельном потоке
+    threading.Thread(target=start_bot, daemon=True).start()  # Запуск Telegram-бота в отдельном потоке
     app.run(host="0.0.0.0", port=5000)  # Запуск Flask
